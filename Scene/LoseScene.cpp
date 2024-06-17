e1120c75e4a1df96e173695120774d143643ee78
Wujiu <a0989680662@gmail.com>
2024-06-17 21:03:33 +0800

    test

--- Scene/LoseScene.cpp
+++ Scene/LoseScene.cpp
@@ -22,7 +22,7 @@ void LoseScene::Initialize() {
 	btn = new Engine::ImageButton("win/dirt.png", "win/floor.png", halfW - 200, halfH * 7 / 4 - 50, 400, 100);
 	btn->SetOnClickCallback(std::bind(&LoseScene::BackOnClick, this, 2));
 	AddNewControlObject(btn);
-	AddNewObject(new Engine::Label("Back", "pirulen.ttf", 48, halfW, halfH * 7 / 4, 0, 0, 0, 255, 0.5, 0.5));
+	AddNewObject(new Engine::Label("Back", "pirulen.ttf", 49, halfW, halfH * 7 / 4, 0, 0, 0, 255, 0.5, 0.5));
     bgmInstance = AudioHelper::PlaySample("astronomia.ogg", false, AudioHelper::BGMVolume, PlayScene::DangerTime);
 }
 void LoseScene::Terminate() {
